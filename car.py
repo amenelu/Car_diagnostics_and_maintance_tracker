62cd9443979538f7cfff6609e0480fdb08d1b967
amen <fikruamen11@gmail.com>
2025-08-11 14:12:32 +0300

    Add initial implementation of Car class and create empty files for diagnostics, main, maintenance, and utils modules

---
+++ car.py
@@ -0,0 +1,9 @@
+class Car:
+    def_init_(self,make,model,year,milage,vin):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.milage=milage
+        self.vin=vin
+        self.menntnce_logs=[]
+        
