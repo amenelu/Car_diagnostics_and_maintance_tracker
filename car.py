ccda7c7a0a3c9a1a8cadf0dc9ac73fa930152af2
amen <fikruamen11@gmail.com>
2025-08-11 21:33:12 +0300

    added datetime import and needs maintance handeling methods

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Car:
     def_init_(self,make,model,year,milage,vin):
         self.make=make
@@ -6,7 +8,7 @@ class Car:
         self.milage=milage
         self.vin=vin
         self.menntnce_logs=[]
-    def add_milge(self,miles):
+    def add_milage(self,miles):
         if miles>0:
             miles+= self.milage
         else:
@@ -26,3 +28,22 @@ class Car:
         self.maintence_logs.append(log)
     def get_maintance_history(self):
         return sorted(self.maintence_logs,key=lambda x:x['date'])
+    
+    def needs_maintance(self,service_interval):
+        if not self.maintence_log:
+            return True;
+        last_service=max(self.maintence_log,key=lambda x:x['date'])
+
+        # wirh respect to milage
+        milage_gap=self.milage - last_service['milage']
+        if milage >= 10000:
+            return True
+
+        # with respect to time gap
+        last_service_date=datetime.datetime.striptime(last_service['date'],
+        "%Y-%m-%d").date()
+        date_since_service=(datetime.date.today()-last_service_date).days
+        if date_since_service >180:
+            return True
+
+        return False
