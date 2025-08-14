3fa5f9ff4c8db9aa4ce43cd88d9df335e796e850
amen <fikruamen11@gmail.com>
2025-08-14 23:43:22 +0300

    typos corrected

--- car.py
+++ car.py
@@ -1,51 +1,54 @@
 import datetime
 
 class Car:
-    def_init_(self,make,model,year,milage,vin):
-        self.make=make
-        self.model=model
-        self.year=year
-        self.milage=milage
-        self.vin=vin
-        self.maintence_logs=[]
-    def add_milage(self,miles):
-        if miles>0:
-            miles+= self.milage
+    def __init__(self, make, model, year, mileage, vin):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+        self.vin = vin
+        self.maintenance_logs = []
+
+    def add_mileage(self, miles):
+        if miles > 0:
+            self.mileage += miles
         else:
-            rasie value error('miles must be positive')
-    
-    def log_maintance(self,service_type,coast,date=None):
+            raise ValueError("Miles must be positive")
+
+    def log_maintenance(self, service_type, cost, date=None):
         if date is None:
-            date=datetime.date.today.isoformat()
-        
-        log={
-            "service":service_type,
-            "cost":cost,
-            "milage":self.milage,
-            "date":date
+            date = datetime.date.today().isoformat()
+
+        log = {
+            "service": service_type,
+            "cost": cost,
+            "mileage": self.mileage,
+            "date": date
         }
 
-        self.maintence_logs.append(log)
-    def get_maintance_history(self):
-        return sorted(self.maintence_logs,key=lambda x:x['date'])
-    
-    def needs_maintance(self,service_interval):
-        if not self.maintence_log:
-            return True;
-        last_service=max(self.maintence_log,key=lambda x:x['date'])
+        self.maintenance_logs.append(log)
 
-        # wirh respect to milage
-        milage_gap=self.milage - last_service['milage']
-        if milage >= 10000:
+    def get_maintenance_history(self):
+        return sorted(self.maintenance_logs, key=lambda x: x['date'])
+
+    def needs_maintenance(self, service_interval):
+        if not self.maintenance_logs:
             return True
 
-        # with respect to time gap
-        last_service_date=datetime.datetime.striptime(last_service['date'],
-        "%Y-%m-%d").date()
-        date_since_service=(datetime.date.today()-last_service_date).days
-        if date_since_service >180:
+        last_service = max(self.maintenance_logs, key=lambda x: x['date'])
+
+        # Check mileage gap
+        mileage_gap = self.mileage - last_service['mileage']
+        if mileage_gap >= service_interval:
+            return True
+
+        # Check time gap
+        last_service_date = datetime.datetime.strptime(last_service['date'], "%Y-%m-%d").date()
+        days_since_service = (datetime.date.today() - last_service_date).days
+        if days_since_service > 180:
             return True
 
         return False
+
     def __str__(self):
-        return f"{self.year} {self.make} {self.model}  {self.vin} {self.milage}miles"
+        return f"{self.year} {self.make} {self.model} (VIN: {self.vin}, Mileage: {self.mileage})"
